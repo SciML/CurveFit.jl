39202fc0804fcc20aed9efcf6f18f7c1e548f03e
JamesWrigley <james@puiterwijk.org>
2026-03-02 17:00:01 +0100

    Add a simple precompilation workload
    This more than doubles the precompilation time, but also about halves the TTFX
    of a simple solve.

--- src/CurveFit.jl
+++ src/CurveFit.jl
@@ -4,6 +4,7 @@ using CommonSolve: CommonSolve, init, solve!, solve
 using ConcreteStructs: @concrete
 using InverseFunctions: inverse
 using Markdown: @doc_str
+using PrecompileTools: @compile_workload, @setup_workload
 using Setfield: @set, @set!
 
 using RecursiveArrayTools: NamedArrayPartition
@@ -49,5 +50,21 @@ export solve, solve!, init
 
 export coef, residuals, predict, fitted, nobs, dof, dof_residual, rss, mse, vcov, stderror, margin_error, confint
 export isconverged
+
+@setup_workload begin
+    x_range = 1.0:10.0
+    x_array = collect(1.0:10.0)
+    fn(a, x) = @. a[1] + a[2] * x^a[3]
+    a0 = [3.0, 2.0, 0.7]
+    y = fn(a0, x_range)
+
+    @compile_workload begin
+        prob = NonlinearCurveFitProblem(fn, [0.5, 0.5, 0.5], x_range, y)
+        solve(prob)
+
+        prob = NonlinearCurveFitProblem(fn, [0.5, 0.5, 0.5], x_array, y)
+        solve(prob)
+    end
+end
 
 end
